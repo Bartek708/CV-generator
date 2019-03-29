5c97e514ecb86891cc7537887e96d82d0a6db188
bartek708 <bartek_starwars@hotmail.com>
2019-03-29 09:34:14 +0100

    toNewBranch

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
 var app = express();
 var path = require('path');
+//var data = require('./data.json');
 const fs = require('fs');
 app.use(express.static('resources'));
+//const pug = require('pug');
 
 app.set('views', './views')
 app.set('view engine', 'pug');
 
+
 let rawData = fs.readFileSync("data.json");
 const data = JSON.parse(rawData);
+
+
+
 
 app.get('/', (req, res) => {
     res.render('index', data );
 });
 
+
 app.get('/data', (req, res) => {
  res.json(data)
 });
+
 
 
 app.use("/static",
